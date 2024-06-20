a2f61ff2c34e9a800ac6f0bd109fe3c5ca6b7e78
Надирабегим <141457772+nadirabegim9@users.noreply.github.com>
2024-06-20 10:03:20 +0600

    Update wsgi.py

--- money_trace/money_trace/wsgi.py
+++ money_trace/money_trace/wsgi.py
@@ -6,11 +6,19 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
-
 import os
+import sys
+#
+## assuming your django settings file is at '/home/MoneyTrace/mysite/mysite/settings.py'
+## and your manage.py is is at '/home/MoneyTrace/mysite/manage.py'
+path = '/home/Moneey/MONEY_TRACE/money_trace'
+if path not in sys.path:
+    sys.path.append(path)
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'money_trace.settings'
+#
+## then:
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'money_trace.settings')
-
 application = get_wsgi_application()
+
+
